bd035c05fb5f9e985369a66d3541fe369ee6f8e6
Raphael Cassio de Souza <rapha.cassio@gmail.com>
2021-07-16 10:13:23 -0300

    Changed migrations

--- src/app/database/migrations/20210607154242_users.ts
+++ src/app/database/migrations/20210607154242_users.ts
@@ -4,8 +4,8 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('users', (table) => {
     table.uuid('id').notNullable().primary();
     table.string('name').notNullable();
-    table.dateTime('created_at').notNullable();
-    table.dateTime('updated_at').notNullable();
+    table.dateTime('created_at').notNullable().defaultTo(knex.fn.now());
+    table.dateTime('updated_at').notNullable().defaultTo(knex.fn.now());
   })
 }
 
