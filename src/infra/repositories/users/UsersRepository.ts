5b131f184472548924fa85cbae81bcb60c548944
Raphael Cassio de Souza <rapha.cassio@gmail.com>
2021-07-16 10:12:21 -0300

    Fix imports

--- src/infra/repositories/users/UsersRepository.ts
+++ src/infra/repositories/users/UsersRepository.ts
@@ -1,25 +1,32 @@
-import { db } from '../../sequelize';
-import { IUser } from '../../sequelize/models';
-import { ICreateUserDto } from '../dtos/ICreateUserDto';
-import { IUsersRepository } from './IUsersRepository';
+import { Repository, getRepository } from 'typeorm';
 
-const { User } = db;
+import { User } from '../../typeorm/entities';
+import { ICreateUserDto } from './dtos/ICreateUserDto';
+import { IUsersRepository } from './interfaces/IUsersRepository';
 
 class UsersRepository implements IUsersRepository {
-  public async findAll(): Promise<IUser[]> {
-    const users = await User.findAll();
+  private respository: Repository<User>;
+
+  constructor() {
+    this.respository = getRepository(User);
+  }
+
+  public async findAll(): Promise<User[]> {
+    const users = await this.respository.find();
 
     return users;
   }
 
-  public async findById(id: string): Promise<IUser> {
-    const user = await User.findByPk(id);
+  public async findById(id: string): Promise<User> {
+    const user = await this.respository.findOne(id);
 
     return user;
   }
 
-  public async create({ name }: ICreateUserDto): Promise<IUser> {
-    const user = await User.create({ name });
+  public async create(data: ICreateUserDto): Promise<User> {
+    const user = this.respository.create(data);
+
+    await this.respository.save(user);
 
     return user;
   }
--- src/infra/repositories/users/fakes/UsersFakeRepository.ts
+++ src/infra/repositories/users/fakes/UsersFakeRepository.ts
@@ -1,22 +1,22 @@
 import { v4 as uuidv4 } from 'uuid';
 
-import { IUser } from '../../../sequelize/models';
-import { ICreateUserDto } from '../../dtos/ICreateUserDto';
-import { IUsersRepository } from '../IUsersRepository';
+import { User } from '../../../typeorm/entities';
+import { ICreateUserDto } from '../dtos/ICreateUserDto';
+import { IUsersRepository } from '../interfaces/IUsersRepository';
 
 class UsersFakeRepository implements IUsersRepository {
-  users: IUser[] = [];
+  users: User[] = [];
 
-  async findAll(): Promise<IUser[]> {
+  async findAll(): Promise<User[]> {
     return this.users;
   }
 
-  async findById(id: string): Promise<IUser> {
+  async findById(id: string): Promise<User> {
     return this.users.find((user) => user.id === id);
   }
 
-  async create({ name }: ICreateUserDto): Promise<IUser> {
-    const user: IUser = {
+  async create({ name }: ICreateUserDto): Promise<User> {
+    const user: User = {
       id: uuidv4(),
       name,
       createdAt: new Date(),
